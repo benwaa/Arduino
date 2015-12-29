d6750cf2b2e576fffc313258aaebf0707c990a92
Ben <cordaben@gmail.com>
2015-12-29 00:49:17 -0500

    fixes

--- wateringLocal/wateringLocal.ino
+++ wateringLocal/wateringLocal.ino
@@ -1,4 +1,5 @@
 #include <stdarg.h>
+#include <Servo.h>
 
 void debug_print(char const*fmt, ... ) {
   char buf[128]; // resulting string limited to 128 chars
@@ -11,22 +12,27 @@ void debug_print(char const*fmt, ... ) {
 }
 
 #define kMAXHUMIDITY 1023.0
+
+#define SERVO_PIN 2
+
 #define BASIL_1_INPUT_PIN 8
 #define BASIL_1_SENSOR_PIN A1
+#define BASIL_1_WATER_ANGLE 0
 #define BASIL_1_THRESHOLD 45
 
-#define BASIL_2_INPUT_PIN 7
-#define BASIL_2_SENSOR_PIN A2
+#define BASIL_2_INPUT_PIN 8 //7
+#define BASIL_2_SENSOR_PIN A1 //A2
+#define BASIL_2_WATER_ANGLE 90
 #define BASIL_2_THRESHOLD 40
 
-const unsigned long kSECOND = 1000;
-const unsigned long kMIN = 60 * kSECOND;
-const unsigned long kHOUR = 60 * kMIN;
-const unsigned long kProbeInterval = 8 * kMIN; // stream intervals in milliseconds
+#define kSECOND 1000
+#define kMIN 60 * kSECOND
+#define kHOUR 60 * kMIN
+#define kWaterTime 30 * kSECOND
 const unsigned int nbRunningAvg = 5; // number of point to compute the avg over
 const int motorPin = A0;
 
-#define DEBUG 1
+#define DEBUG 0
 #if DEBUG
 #define INTERVAL_TIME 5*kSECOND
 #define debug(format, ...) \
@@ -36,9 +42,10 @@ const int motorPin = A0;
 #define debug(format, ...) do {} while (0)
 #endif
 
-
+Servo myservo;
 void setup() {
   Serial.begin(9600);
+  myservo.attach(SERVO_PIN);
   pinMode(motorPin, OUTPUT);
   digitalWrite(motorPin, LOW);
   pinMode(BASIL_1_INPUT_PIN, OUTPUT);
@@ -50,36 +57,55 @@ void loop() {
   basil1();
   basil2();
   delay(INTERVAL_TIME);
-  stopWatering();
 }
 
 void basil1() {
   int avgMoistValue = RecomputeAverageMoisture(BASIL_1_INPUT_PIN, BASIL_1_SENSOR_PIN);
   debug("basil1 avg moisture:%i", avgMoistValue);
   if (shouldWater(1, avgMoistValue)) {
-    startWatering(1);
+    watering(1);
   }
 }
 
 void basil2() {
-  int avgMoistValue = RecomputeAverageMoisture(BASIL_1_INPUT_PIN, BASIL_1_SENSOR_PIN);
-  debug("basil1 avg moisture:%i", avgMoistValue);
-  if (shouldWater(1, avgMoistValue)) {
-    startWatering(1);
+  int avgMoistValue = RecomputeAverageMoisture(BASIL_2_INPUT_PIN, BASIL_2_SENSOR_PIN);
+  debug("basil2 avg moisture:%i", avgMoistValue);
+  if (shouldWater(2, avgMoistValue)) {
+    watering(2);
+  }
+}
+
+void driveWater(int plant) {
+  switch (plant) {
+  case 1:
+    myservo.write(BASIL_1_WATER_ANGLE);
+    break;
+  case 2:
+    myservo.write(BASIL_2_WATER_ANGLE);
+    break;
   }
 }
 
 bool shouldWater(int plant, int moisture) {
+  switch (plant) {
+  case 1:
+    if (moisture <= BASIL_1_THRESHOLD) return true;
+    break;
+  case 2:
+    if (moisture <= BASIL_2_THRESHOLD) return true;
+    break;
+  }
   return false;
 }
 
-void startWatering(int plant) {
+void watering(int plant) {
+  driveWater(plant);
+  delay(kSECOND);
+  debug("start watering %d", plant);
   digitalWrite(motorPin, HIGH);
-  debug("start watering");
-}
-void stopWatering() {
-  debug("stop watering");
+  delay(kWaterTime);
   digitalWrite(motorPin, LOW);
+  debug("stop watering");
 }
 
 int RecomputeAverageMoisture(int inputPIN, int sensorPIN) {
